af44282f64d15fc7ff51c8e973635862f8378c7b
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-03-01 12:08:29 +0700

    Day01: React BTVN

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,9 +1,22 @@
-const data = [
-	{ id: 1, title: "T-shirt", price: 100, description: "This is a T-shirt" },
-	{ id: 2, title: "Shirt", price: 200, description: "This is a Shirt" },
-	{ id: 3, title: "Pants", price: 300, description: "This is a Pants" },
-	{ id: 4, title: "Shoes", price: 400, description: "This is a Shoes" },
-];
+import React from "react";
+import ProductItem from "../components/ui/ProductItem";
+import data from "../data";
+
+function ShopPage() {
+	console.log(data);
+
+	// Các hooks cần được sử dụng trong function component
+	return (
+		<div>
+			{
+				// Hiển thị danh sách sản phẩm
+				<ProductItem />
+			}
+		</div>
+	);
+}
+
+export default ShopPage;
 
 // Xây dựng ShopPage component hiển thị danh sách sản phẩm từ data trên
 /**
